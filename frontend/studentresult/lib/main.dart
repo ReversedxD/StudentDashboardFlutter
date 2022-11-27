5b403dc57efef1f1439b661cf83e2a4a546b978f
ReversedxD <dudejasiddharth@gmail.com>
2022-11-28 05:21:55 +0530

    final changes

--- frontend/studentresult/lib/main.dart
+++ frontend/studentresult/lib/main.dart
@@ -5,7 +5,105 @@ import 'package:http/http.dart' as http;
 import 'package:json_table/json_table.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    home: InitialPage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class InitialPage extends StatelessWidget {
+  const InitialPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Testing Django',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 235, 220, 199),
+        appBar: AppBar(
+          leading: CircleAvatar(
+            child: Image.network(
+                'https://www.bennett.edu.in/wp-content/themes/twentysixteen/cdn/images/logo.png'),
+          ),
+          centerTitle: true,
+          title: const Text(
+            "Student Results",
+            style: TextStyle(
+              fontFamily: 'Source Sans Pro',
+            ),
+          ),
+        ),
+        body: SafeArea(
+            child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(33.0),
+            child: Column(
+              children: [
+                Card(
+                  color: Colors.orange.shade200,
+                  child: const Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Text(
+                      'Bennett University Student Results Platform',
+                      style: TextStyle(
+                        fontSize: 35.0,
+                        color: Color.fromARGB(255, 63, 63, 63),
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Source Sans Pro',
+                        // decoration: TextDecoration.underline,
+                        // decorationStyle: TextDecorationStyle.solid,
+                        // decorationColor: Colors.orange.shade500,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 60.0,
+                ),
+                ElevatedButton.icon(
+                  onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return const MyApp();
+                      },
+                    ),
+                  ),
+                  label: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      "Click here to view your results",
+                      style: TextStyle(
+                        fontSize: 22.0,
+                        color: Color.fromARGB(255, 63, 63, 63),
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Source Sans Pro',
+                        decoration: TextDecoration.none,
+                        decorationStyle: TextDecorationStyle.solid,
+                      ),
+                    ),
+                  ),
+                  icon: Icon(Icons.web),
+                  // style: ElevatedButton.styleFrom(
+                  //   textStyle: const TextStyle(
+                  //     fontSize: 30,
+                  //     fontFamily: 'Source Sans Pro',
+                  //     letterSpacing: 1,
+                  //     height: 4.0,
+                  //     // backgroundColor: Colors.orange.shade400),
+                  //   ),
+                ),
+              ],
+            ),
+          ),
+        )),
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -31,11 +129,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Testing Django',
+      debugShowCheckedModeBanner: false,
+      title: 'Results Viewer',
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
       home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 235, 220, 199),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return const InitialPage();
+              },
+            ),
+          ),
+          label: const Text('Go back'),
+          icon: const Icon(Icons.arrow_back),
+        ),
         appBar: AppBar(
           centerTitle: true,
           title: const Text("Student Results"),
@@ -43,10 +155,26 @@ class MyApp extends StatelessWidget {
         body: Center(
           child: Column(
             children: [
-              const Padding(
+              Padding(
                 padding: EdgeInsets.all(16),
-                child: Text("Here is your result for E20CSE302",
-                    style: TextStyle(fontSize: 20)),
+                child: Card(
+                  color: Colors.orange.shade200,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Here's your result for E20CSE302",
+                      style: TextStyle(
+                        fontSize: 35.0,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Source Sans Pro',
+                        // decoration: TextDecoration.underline,
+                        // decorationStyle: TextDecorationStyle.solid,
+                        decorationColor: Colors.orange.shade500,
+                      ),
+                    ),
+                  ),
+                ),
               ),
               FutureBuilder<Map<String, dynamic>>(
                 builder: ((context, snapshot) {
@@ -54,12 +182,12 @@ class MyApp extends StatelessWidget {
                     return Expanded(
                         child: ListView(
                       children: [
-                        Padding(
+                        const Padding(
                           padding: const EdgeInsets.all(16.0),
                           child: Align(
                             alignment: Alignment.center,
                             child: Text(
-                              "Total Marks: ${snapshot.data!['total_marks']}",
+                              'Subject-wise marks of the current semester:',
                               style: const TextStyle(fontSize: 20),
                             ),
                           ),
@@ -67,7 +195,51 @@ class MyApp extends StatelessWidget {
                         JsonTable(
                           snapshot.data!["subjects"]!,
                           allowRowHighlight: true,
-                        )
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Card(
+                          color: Colors.orange.shade300,
+                          child: Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Center(
+                              child: Text(
+                                "Total Marks: ${snapshot.data!['total_marks']}",
+                                style: TextStyle(
+                                  fontSize: 15.0,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Source Sans Pro',
+                                  decoration: TextDecoration.underline,
+                                  decorationStyle: TextDecorationStyle.solid,
+                                  decorationColor: Colors.orange.shade500,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Card(
+                          color: Colors.orange.shade400,
+                          child: const Center(
+                            child: Text(
+                              'Refer to the below Marks-Grade comparison for your referece',
+                              style: TextStyle(
+                                fontSize: 25.0,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Source Sans Pro',
+                              ),
+                            ),
+                          ),
+                        ),
+                        // const SizedBox(
+                        //   height: 10,
+                        // ),
+                        Image.network('https://i.imgur.com/h9puh3z.png')
                       ],
                     ));
                   } else {
@@ -76,13 +248,6 @@ class MyApp extends StatelessWidget {
                 }),
                 future: buttonPressed(),
               ),
-              // Padding(
-              //   padding: EdgeInsets.all(10),
-              //   child: ElevatedButton(
-              //     onPressed: totalee,
-              //     child: Text("Here are your total marks"),
-              //   ),
-              // )
             ],
           ),
         ),
